Major commit of various files. growing_season.py: corrected the diurnal cycle, added the growth respi, validated the unit conversion of the carbon fluxes. toolbox.py: corrected 2 functions. configuration file: added DMI as an output variable. plotting file: added a function to plot fluxnet data, and developed a function to plot shapefile maps. retrieve_DM.py: corrected the value of potatoe DM. optimize.py: added and assertion in case the observed yields are too high.

growing_season_C_fluxes.py
@@ -115,10 +115,10 @@ def main():
 
 
         if (prod_figure == True):
-            from matplotlib import pyplot
-            pyplot.close('all')
-            fig1 = pyplot.figure('GPP per stu',figsize=(14,6))
-            fig1.subplots_adjust(0.1,0.2,0.98,0.85,0.4,0.6)
+            from matplotlib import pyplot as plt
+            plt.close('all')
+            fig1, axes = plt.subplots(nrows=3, ncols=1, figsize=(14,10))
+            fig1.subplots_adjust(0.1,0.1,0.98,0.9,0.2,0.2)
 
 #-------------------------------------------------------------------------------
 #       WE NEED TO LOOP OVER THE SOIL TYPE
@@ -135,8 +135,10 @@ def main():
 # We apply the short wave radiation diurnal cycle on the GPP and R_auto
 
             # we create empty time series for this specific stu
-            gpp_stu_timeseries  = np.array([])
-            raut_stu_timeseries = np.array([])
+            gpp_cycle_timeseries   = np.array([])
+            raut_cycle_timeseries  = np.array([])
+            gpp_perday_timeseries  = np.array([])
+            raut_perday_timeseries = np.array([])
 
             # we compile the sum of the stu areas to do a weighted average of
             # GPP and Rauto later on
@@ -154,17 +156,19 @@ def main():
                 test_rip = (time - wofost_data[filelist]['day'][-1]).total_seconds() 
                 #print 'tests:', test_sow, test_rip
 
-                # if the day of the time series is before sowing date: npp=0
+                # if the day of the time series is before sowing date: plant 
+                # fluxes are set to zero
                 if test_sow < 0.: 
                     gpp_day  = 0.
                     raut_day = 0.
-                # if the day of the time series is after the harvest date: npp=0
+                # or if the day of the time series is after the harvest date: 
+                # plant fluxes are set to zero
                 elif test_rip > 0.: 
                     gpp_day  = 0.
                     raut_day = 0.
-                # else we get the daily total GPP and Raut in kgCH2O/ha/day from 
-                # wofost, and we weigh it with the stu area to later on calculate
-                # the weighted average GPP and Raut in the grid cell
+                # else we get the daily total GPP and Raut in kgCH2O/ha/day
+                # from wofost, and we weigh it with the stu area to later on 
+                # calculate the weighted average GPP and Raut in the grid cell
                 else: 
                     # index of the sowing date in the time_cell_timeseries:
                     if (test_sow == 0.): DOY_sowing  = DOY
@@ -175,53 +179,67 @@ def main():
                     #print 'index of day in wofost record:', index_day_w
 
                     # unit conversion: from kgCH2O/ha/day to gC/m2/day
-                    gpp_day   = - wofost_data[filelist]['GASS'][index_day_w] * \
-                                             stu_area * (mmC / mmCH2O) * 0.1
-                    raut_day  = wofost_data[filelist]['MRES'][index_day_w] * \
-                                             stu_area * (mmC / mmCH2O) * 0.1
+                    gpp_day    = - wofost_data[filelist]['GASS'][index_day_w] * \
+                                                            (mmC / mmCH2O) * 0.1
+                    maint_resp = wofost_data[filelist]['MRES'][index_day_w] * \
+                                                            (mmC / mmCH2O) * 0.1
+                    try: # if there are any available assimilates for growth
+                        growth_fac = (wofost_data[filelist]['DMI'][index_day_w]) / \
+                                 (wofost_data[filelist]['GASS'][index_day_w] - 
+                                  wofost_data[filelist]['MRES'][index_day_w])
+                        growth_resp = (1.-growth_fac)*(-gpp_day-maint_resp) 
+                    except ZeroDivisionError: # otherwise there is no crop growth
+                        growth_resp = 0.
+                    raut_day   = growth_resp + maint_resp
 
                 # we select the radiation diurnal cycle for that date
                 # NB: the last index is ignored in the selection, so we DO have
                 # 8 time steps selected only (it's a 3-hourly dataset)
-                rad_cycle   = rad[1][DOY*8:DOY*8+8] 
+                rad_cycle      = rad[1][DOY*8:DOY*8+8] 
 
                 # we apply the radiation cycle on the GPP and Rauto
                 # and we transform the daily integral into a rate
-                weights       = rad_cycle / sum(rad_cycle)
-                # average gpp and raut rates per second:
-                dt            = 3600. * 24.
-                ave_gpp_rate  = gpp_day   / dt
-                ave_raut_rate = raut_day  / dt 
-                # the day's time series of actual gpp and raut rates per second:
-                gpp_cycle     = weights * ave_gpp_rate
-                raut_cycle    = weights * ave_raut_rate
+                weights        = rad_cycle / sum(rad_cycle)
+                # the sum of the 8 rates is equal to total/dt:
+                dt             = 3600. * 3.
+                sum_gpp_rates  = gpp_day   / dt
+                sum_raut_rates = raut_day  / dt 
+                # the day's 8 values of actual gpp and raut rates per second:
+                gpp_cycle      = weights * sum_gpp_rates
+                raut_cycle     = weights * sum_raut_rates
                 # NB: we check if the applied diurnal cycle is correct
-                if (sum(gpp_cycle)*dt-gpp_day) > 0.01: 
-                    print 'wrong kernel applied on GPP', \
-                           sum(gpp_cycle)*dt- gpp_day, sum(weights), DOY
-                if (sum(raut_cycle)*dt-raut_day) > 0.01: 
-                    print 'wrong kernel applied on Rauto', \
-                           sum(raut_cycle)*dt-raut_day, sum(weights), DOY
-                gpp_stu_timeseries  = np.concatenate((gpp_stu_timeseries, 
-                                                      gpp_cycle), axis=0)
-                raut_stu_timeseries = np.concatenate((raut_stu_timeseries,
-                                                      raut_cycle), axis=0)
+                assert (sum(weights)-1. < 0.000001), "wrong radiation kernel"
+                assert (len(gpp_cycle)*int(dt) == 86400), "wrong dt in diurnal cycle"
+                assert ((sum(gpp_cycle)*dt-gpp_day) < 0.00001), "wrong diurnal cycle "+\
+                    "applied on GPP: residual=%.2f "%(sum(gpp_cycle)*dt-gpp_day) +\
+                    "on DOY %i"%DOY
+                assert ((sum(raut_cycle)*dt-raut_day) < 0.00001), "wrong diurnal cycle "+\
+                    "applied on Rauto: residual=%.2f "%(sum(raut_cycle)*dt-raut_day) +\
+                    "on DOY %i"%DOY
+
+                # if the applied diurnal cycle is ok, we append that day's cycle
+                # to the yearly record of the stu
+                gpp_cycle_timeseries  = np.concatenate((gpp_cycle_timeseries, 
+                                                       gpp_cycle), axis=0)
+                raut_cycle_timeseries = np.concatenate((raut_cycle_timeseries,
+                                                       raut_cycle), axis=0)
+                # we also store the carbon fluxes per day, for comparison with fluxnet
+                gpp_perday_timeseries = np.concatenate((gpp_perday_timeseries,
+                                                       [gpp_day]), axis=0) 
+                raut_perday_timeseries = np.concatenate((raut_perday_timeseries,
+                                                       [raut_day]), axis=0)
 
 
             if (prod_figure == True):
-                pyplot.plot(time_cell_timeseries/(3600.*24.),gpp_stu_timeseries*\
-                                          1000./stu_area, label='stu %i'%stu_no)
-        if (prod_figure == True):
-            pyplot.xlim([40.,170.])
-            pyplot.xlabel('time (DOY)')
-            pyplot.ylabel(r'carbon flux (mg$_{C}$ m$^{-2}$ s$^{-1}$)')
-            pyplot.legend(loc='best', ncol=2, fontsize=10)
-            pyplot.title('Carbon fluxes of %s over the  '%crop_name+\
-                         'cultivated area of grid cell %i (%s) in %i'%(grid_no,
-                                                            NUTS_no, opti_year))
-            figname = 'GPP_per_stu_c%s_%s_y%i_g%i.png'%(crop_no,NUTS_no,opti_year,\
-                                                                        grid_no)
-            #fig1.savefig(figname)
+                for ax, var, name, lims in zip(axes.flatten(), 
+                [gpp_perday_timeseries, raut_perday_timeseries, 
+                gpp_perday_timeseries + raut_perday_timeseries],
+                ['GPP', 'Rauto', 'NPP'], [[-20.,0.],[0.,10.],[-15.,0.]]):
+                    ax.plot(time_cell_timeseries[::8]/(3600.*24.), var, 
+                                                          label='stu %i'%stu_no)
+                    ax.set_xlim([40.,170.])
+                    ax.set_ylim(lims)
+                    ax.set_ylabel(name + r' (g$_{C}$ m$^{-2}$ d$^{-1}$)', fontsize=14)
 
 #-------------------------------------------------------------------------------
 # We add the gpp of all soil types in the grid cell. NB: different calendars
@@ -229,8 +247,19 @@ def main():
 # differ from stu to stu
 
             # sum the timeseries of GPP and Rauto for all soil types
-            gpp_cell_timeseries  = gpp_cell_timeseries  + gpp_stu_timeseries
-            raut_cell_timeseries = raut_cell_timeseries + raut_stu_timeseries
+            gpp_cell_timeseries  = gpp_cell_timeseries  + gpp_cycle_timeseries*stu_area
+            raut_cell_timeseries = raut_cell_timeseries + raut_cycle_timeseries*stu_area
+
+        # finish the figure of multiple stu carbon fluxes
+        if (prod_figure == True):
+            plt.xlabel('time (DOY)', fontsize=14)
+            plt.legend(loc='upper left', ncol=2, fontsize=10)
+            fig1.suptitle('Daily carbon fluxes of %s for all '%crop_name+\
+                         'soil types of grid cell %i (%s) in %i'%(grid_no,
+                                                 NUTS_no, opti_year), fontsize=18)
+            figname = 'GPP_perday_c%s_%s_y%i_g%i.png'%(crop_no,NUTS_no,opti_year,\
+                                                                        grid_no)
+            fig1.savefig(figname)
 
         # for each grid cell, calculate the weighted average GPP and Rauto
         gpp_cell_timeseries  = gpp_cell_timeseries  / sum_stu_areas
@@ -262,28 +291,29 @@ def main():
 # if requested by the user, we produce one NEE figure per grid cell
  
         if (prod_figure == True):
-            from matplotlib import pyplot
-            fig2 = pyplot.figure(figsize=(14,3))
+            from matplotlib import pyplot as plt
+            fig2 = plt.figure(figsize=(14,6))
             fig2.subplots_adjust(0.1,0.2,0.98,0.85,0.4,0.6)
-            pyplot.plot(time_cell_timeseries/(3600.*24.),gpp_cell_timeseries*1000., 
-                                                             label='GPP', c='g')
-            pyplot.plot(time_cell_timeseries/(3600.*24.),raut_cell_timeseries*1000.,
+            plt.plot(time_cell_timeseries/(3600.*24.),gpp_cell_timeseries*1000., 
+                                                             label=r'$\mathrm{GPP}$', c='g')
+            plt.plot(time_cell_timeseries/(3600.*24.),raut_cell_timeseries*1000.,
                                                       label=r'$R_{aut}$', c='b')
-            pyplot.plot(time_cell_timeseries/(3600.*24.),rhet_cell_timeseries*1000.,
+            plt.plot(time_cell_timeseries/(3600.*24.),rhet_cell_timeseries*1000.,
                                                       label=r'$R_{het}$', c='r')
-            pyplot.plot(time_cell_timeseries/(3600.*24.),nee_cell_timeseries*1000., 
-                                                             label='NEE', c='k')
-            pyplot.xlim([40.,170.])
-            pyplot.xlabel('time (DOY)')
-            pyplot.ylabel(r'carbon flux (mg$_{C}$ m$^{-2}$ s$^{-1}$)')
-            pyplot.legend(loc='best', ncol=2, fontsize=10)
-            pyplot.title('Average carbon fluxes of %s over the  '%crop_name+\
+            plt.plot(time_cell_timeseries/(3600.*24.),nee_cell_timeseries*1000., 
+                                                             label=r'$\mathrm{NEE}$', c='k')
+            plt.xlim([0.,365.])
+            plt.ylim([-1.,1.])
+            plt.xlabel('time (DOY)')
+            plt.ylabel(r'carbon flux (mg$_{C}$ m$^{-2}$ s$^{-1}$)')
+            plt.legend(loc='best', ncol=2, fontsize=10)
+            plt.title('Average carbon fluxes of %s over the  '%crop_name+\
                          'cultivated area of grid cell %i (%s) in %i'%(grid_no,
                                                             NUTS_no, opti_year))
             figname = 'Cbalance_c%s_%s_y%i_g%i.png'%(crop_no,NUTS_no,opti_year,\
                                                                         grid_no)
             fig2.savefig(figname)
-            pyplot.show()
+            #plt.show()
 
 #-------------------------------------------------------------------------------
         # until the whole code has been validated, we stop the script after

maries_toolbox.py
@@ -48,6 +48,7 @@ def select_cells(NUTS_no, folder_pickle, method='topn', n=3):
     list_of_tuples = pickle_load(open(filename,'rb'))
     # we select the first item from the file, which is the actual list of tuples
     list_of_tuples = list_of_tuples[0]
+    print list_of_tuples
 
     # first option: we select the top n grid cells in terms of arable land area
     if (method == 'topn'):
@@ -295,16 +296,15 @@ def fetch_EUROSTAT_NUTS_name(NUTS_no, EUROSTATdir):
     # deleting rows that are not data
     del lines[0]
 
-    print lines[1]
-
     # we keep the string format, we just separate the string items
     dictnames = dict()
     for row in lines:
         dictnames[row[0]] = row[1]
 
 # we fetch the NUTS region name corresponding to the code
- 
-    return dictnames[NUTS_no]
+
+    print "EUROSTAT region name of %s:"%NUTS_no, dictnames[NUTS_no].lower()
+    return dictnames[NUTS_no].lower()
 
 #===============================================================================
 # Function to detrend the observed EUROSTAT yields or harvests
@@ -344,7 +344,7 @@ def detrend_obs( _start_year, _end_year, _NUTS_name, _crop_name,
     TARGET = np.array([0.]*nb_years)
     for j,year in enumerate(campaign_years):
         for i,region in enumerate(uncorrected_yields_dict['GEO']):
-            if region.startswith(_NUTS_name[0:12]):
+            if (region.lower()==_NUTS_name):
                 if uncorrected_yields_dict['CROP_PRO'][i]==_crop_name:
                     if (uncorrected_yields_dict['TIME'][i]==str(int(year))):
                         if (uncorrected_yields_dict['STRUCPRO'][i]==
@@ -464,8 +464,8 @@ def open_pcse_csv_output(inpath,filelist):
             datestr = split(line[0], '-')
             a = [date(int(datestr[0]),int(datestr[1]),int(datestr[2])), \
                  float(line[1]), float(line[2]), float(line[3]), float(line[4]), \
-                 float(line[5]), float(line[6]), float(line[7]), float(line[8])]#, \
-                 #float(line[9]), float(line[10]), float(line[11])]
+                 float(line[5]), float(line[6]), float(line[7]), float(line[8]), \
+                 float(line[9])]#, float(line[10]), float(line[11])]
             converted_data.append(a)
         data = np.array(converted_data)
 

optimize.py
@@ -22,8 +22,8 @@ def main():
 # Define the settings of the run:
  
     # NUTS region and crop:
-    NUTS_no       = 'ES43'
-    crop_no       = 3        # CGMS crop number
+    NUTS_no       = 'NL1'
+    crop_no       = 7        # CGMS crop number
 
     # yield gap factor optimization:
     optimization  = True     # if False: we assign a YLDGAPF = 1.
@@ -37,22 +37,6 @@ def main():
     forward_sims  = True
     start_year    = 2006     # start_year and end_year define the period of time
     end_year      = 2006     # for which we do forward simulations
-
-#-------------------------------------------------------------------------------
-# Calculate key variables from the user input
-
-    # we create an array containing the years for which we do forward runs:
-    nb_years      = int(end_year - start_year + 1.)
-    campaign_years = np.linspace(int(start_year),int(end_year),nb_years)
-
-    #we fetch the EUROSTAT region name corresponding to the NUTS_no
-    NUTS_name = fetch_EUROSTAT_NUTS_name(NUTS_no, EUROSTATdir)
-
-    # we fetch the EUROSTAT crop name corresponding to the CGMS crop_no
-    # 1-we retrieve both the CGMS and EUROSTAT names of a list of crops
-    crop_name  = get_crop_name([crop_no])
-    # 2-we select the EUROSTAT name of our specific crop 
-    crop_name  = crop_name[crop_no][1]
 
 #-------------------------------------------------------------------------------
 # Define working directories
@@ -71,6 +55,22 @@ def main():
     #EUROSTATdir   = "/Users/mariecombe/Cbalance/EUROSTAT_data"
     #folderpickle  = '/Storage/CO2/mariecombe/pickled_CGMS_input_data/'
     #pcseoutput    = '/Storage/CO2/mariecombe/pcse_individual_output/'
+
+#-------------------------------------------------------------------------------
+# Calculate key variables from the user input
+
+    # we create an array containing the years for which we do forward runs:
+    nb_years      = int(end_year - start_year + 1.)
+    campaign_years = np.linspace(int(start_year),int(end_year),nb_years)
+
+    #we fetch the EUROSTAT region name corresponding to the NUTS_no
+    NUTS_name = fetch_EUROSTAT_NUTS_name(NUTS_no, EUROSTATdir)
+
+    # we fetch the EUROSTAT crop name corresponding to the CGMS crop_no
+    # 1-we retrieve both the CGMS and EUROSTAT names of a list of crops
+    crop_name  = get_crop_name([crop_no])
+    # 2-we select the EUROSTAT name of our specific crop 
+    crop_name  = crop_name[crop_no][1]
 
 #-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
@@ -326,6 +326,8 @@ def optimize_regional_yldgapf_dyn(crop_no_, frac_crop, selected_grid_cells_,
 
         # 5- we compute the (sim-obs) differences.
         DIFF = TSO_regional - OBS
+        assert (TSO_regional[-1] > OBS[-1]), "Observed yield too high! The DM '+\
+               'content is wrong. Check it again."
         
         # Writing more output
         print '\nIteration %i'%iter_no

plot_optimized_yields.py
@@ -26,15 +26,16 @@ def main():
                     # yield from the observed yield
 
     # figures for the forward simulations:
-    figure_4 = True # figure showing a time series of opt vs. obs yields
-    figure_5 = True # figure showing a map of yield over a region
+    figure_4 = False # figure showing a time series of opt vs. obs yields
+    figure_5 = False # figure showing a map of yield over a region
+    figure_6 = True # comparison with fluxnet data
 
 #-------------------------------------------------------------------------------
 # Define general working directories
     currentdir = os.getcwd()
 	# directories on my local MacBook:
-    EUROSTATdir   = '/Users/mariecombe/Documents/Work/Research_project_3/'\
-				   +'EUROSTAT_data'
+    EUROSTATdir   = '../EUROSTAT_data'
+    FLUXNETdir    = '../FluxNet_data'
     # directories on capegrim:
     #EUROSTATdir   = "/Users/mariecombe/Cbalance/EUROSTAT_data"
 #-------------------------------------------------------------------------------
@@ -54,7 +55,7 @@ def main():
                             # find the results files of the sensitivity analysis
     labels    = ['all', 'top 10', 'top 5', 'top 3'] 
     colors    = ['k','r','b','g']#,'cyan','orange','yellow']
-    RMSEdir   = os.path.join(currentdir, 'output_data') # directory where the
+    RMSEdir   = os.path.join(currentdir, 'pcse_summary_output') # directory where the
                             # sensitivity analysis results files are located
 
     # open the results from the sensitivity analysis
@@ -92,7 +93,7 @@ def main():
     start_year  = 2000     # start_year and end_year define the period of time
     end_year    = 2014     # for which we did our forward simulations
     opti_year   = 2006       # nb of years for which we optimized the YLDGAPF
-    ForwardSimdir = os.path.join (currentdir, 'output_data') # directory where
+    ForwardSimdir = os.path.join (currentdir, 'pcse_summary_output') # directory where
                              # the results files of the forward simulations are
                              # located
 
@@ -145,7 +146,102 @@ def main():
 #-------------------------------------------------------------------------------
 # Plot the figure showing a map of yield over a region
 
-#    if (figure_5 == True):
+    if (figure_5 == True):
+        from mpl_toolkits.basemap import Basemap
+        from matplotlib.patches import Polygon
+        from matplotlib.collections import PatchCollection
+        from matplotlib.patches import PathPatch
+        from osgeo import ogr # to add data to a shapefile
+
+# Define the shapefile path and filename:
+
+        path = '../EUROSTAT_data/EUROSTAT_website_2010_shapefiles/'
+        #filename = 'NUTS_BN_03M_2010'# NUTS regions 
+        filename = 'NUTS_RG_03M_2010_copy'# NUTS regions 
+
+# first add an attribute to the shapefile (make a function for that)
+# this attribute will be the yield data! observed, simulated, anomaly, etc...
+
+        # open a shapefile and get field names
+        source = ogr.Open(os.path.join(path + filename), 1) # 1 is read/write
+        layer = source.GetLayer()
+        layer_defn = layer.GetLayerDefn()
+        field_names = [layer_defn.GetFieldDefn(i).GetName() for i in 
+                       range(layer_defn.GetFieldCount())]
+        print 'nb of fields:', len(field_names), 'yield' in field_names
+        # add a new field
+        new_field = ogr.FieldDefn('yield', ogr.OFTReal) # could be OFTInteger or 
+                                                        # OFTString, depending on
+                                                        # desired data type
+        layer.CreateField(new_field)
+        field_names = [layer_defn.GetFieldDefn(i).GetName() for i in
+                       range(layer_defn.GetFieldCount())]
+        print 'nb of fields:', len(field_names), 'yield' in field_names
+        # close the shapefile
+        source = None
+        sys.exit(2)
+
+# create a map of Europe with coast lines
+
+        fig, ax = plt.subplots(nrows = 1, ncols = 1, figsize=(7,7))
+        map = Basemap(projection='laea', lat_0=48, lon_0=16, llcrnrlat=30, 
+                      llcrnrlon=-10, urcrnrlat=65, urcrnrlon=45)
+        map.drawcoastlines()
+
+# plot specific scatter points (e.g. FluxNet data sites)
+
+        lons = [-5, 0]
+        lats = [40, 48]
+        x, y = map(lons,lats)
+        map.scatter(x, y, marker='D', color='m')
+
+# Read a shapefile and its metadata
+
+        name = 'NUTS'
+        # read the shapefile data WITHOUT plotting its shapes
+        NUTS_info = map.readshapefile(path + filename, name, drawbounds=False) 
+
+# Plot polygons and the attached data:
+
+        # retrieve the list of patches to fill and its data to plot
+        patches    = []
+        yield_data = []
+        for info, shape in zip(map.NUTS_info, map.NUTS):
+            if info['STAT_LEVL_'] == 2: # for NUTS 2 regions only
+                patches.append( Polygon(np.array(shape), True) )
+                yield_data.append( float(info['SHAPE_Area']) )
+
+        # create a color scale that fits the data
+        cmap = plt.get_cmap('RdYlBu')
+        colors = cmap(yield_data)
+
+        # add the patches on the map
+        collection = PatchCollection(patches, cmap = cmap, facecolors=colors, 
+                                     edgecolor='k', linewidths=1., zorder=2) 
+        #collection.set_array(np.array(colors))
+        #collection.set_clim(0.,5.)
+        #plt.colorbar(collection)
+        ax.add_collection(collection)
+       
+        plt.show()
+#-------------------------------------------------------------------------------
+# Plot the FluxNet GPP, Reco from various sites
+
+    if (figure_6 == True):
+        sites = ['DE-Kli', 'ES-ES2']
+        years = [2006]
+        for site in sites:
+            listoffiles = [f for f in os.listdir(FLUXNETdir) if (site in f)]
+            filename = listoffiles[0]
+            FNdata = open_csv(FLUXNETdir, listoffiles)
+            fig = plt.subplots(ncols=1, nrows=1, figsize=(14,7))
+            for var in ["GPP_f","Reco","NEE_f"]:
+                plt.plot(FNdata[filename]['DoY'],FNdata[filename][var])
+            
+            plt.xlabel('Time (DOY)')
+            plt.ylabel('Carbon fluxes (gC m-2 d-1)')
+        #fig.suptitle(site)
+        plt.show()
         
 #===============================================================================
 def plot_precision_yldgapf(list_of_crops, list_of_regions, start_year_, 